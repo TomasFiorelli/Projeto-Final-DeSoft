a95d1a3a4c3018f75db269b25b14778b49296121
gabriellakz <62666958+gabriellakz@users.noreply.github.com>
2020-06-01 10:18:25 -0300

    Update 1v.py

--- 1v.py
+++ 1v.py
@@ -1,6 +1,7 @@
 # ===== Inicialização =====
 # ----- Importa e inicia pacotes
-import pygame, random
+import pygame, random, time
+from pygame.time import delay
 
 pygame.init()
 
@@ -16,9 +17,10 @@ BLOCO_WIDTH = 70
 BLOCO_HEIGHT = 70
 
 class Bloco(pygame.sprite.Sprite):
-    def __init__(self, img):
+    def __init__(self, img, largura):
         pygame.sprite.Sprite.__init__(self)
         
+        img = pygame.transform.scale(img, (largura, BLOCO_HEIGHT))
         self.image = img
         self.rect = self.image.get_rect()
         self.rect.x = -100
@@ -37,31 +39,58 @@ class Bloco(pygame.sprite.Sprite):
         if self.rect.bottom == HEIGHT:
             self.speedx = 0
             self.speedy = 0
+    
+    def corta(self, esquerda, direita):
+        left = max(esquerda, self.rect.left)
+        right = min(direita, self.rect.right)
+        y = self.rect.y
+        self.image = pygame.transform.scale(self.image, (right - left, BLOCO_HEIGHT))
+        self.rect = self.image.get_rect()
+        self.rect.x = left
+        self.rect.y = y
         
 game = True
 clock = pygame.time.Clock()
 FPS = 60
-bloco1 = Bloco(img)
+
+bloco = Bloco(img, BLOCO_WIDTH)
+all_sprites = pygame.sprite.Group()
+all_sprites.add(bloco)
+all_blocks = pygame.sprite.Group()
 
 # ===== Loop principal =====
 while game:
     clock.tick(FPS)
+    
+    hits = pygame.sprite.spritecollide(bloco, all_blocks, False)
+    if bloco.rect.bottom == HEIGHT or len(hits) > 0:
+        if len(hits) > 0:
+            bloco_colisao = hits[0]
+            bloco.corta(bloco_colisao.rect.left, bloco_colisao.rect.right)
+        bloco.speedx = 0
+        bloco.speedy = 0
+        all_blocks.add(bloco)
+        largura = bloco.rect.width
+        bloco = Bloco(img, largura)
+        all_sprites.add(bloco)
+
     # ----- Trata eventos
     for event in pygame.event.get():
         # ----- Verifica consequências
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-                bloco1.speedx = 0
-                bloco1.speedy = 5
+                bloco.speedx = 0
+                bloco.speedy = 5
 
         if event.type == pygame.QUIT:
             game = False
  
-    bloco1.update()
+    all_sprites.update()
 
     # ----- Gera saídas
     window.fill((255, 255, 255))  # Preenche com a cor branco
-    window.blit(bloco1.image, bloco1.rect)
+    all_sprites.draw(window)
+    
 
     # ----- Atualiza estado do jogo
     pygame.display.update()  # Mostra o novo frame para o jogador
