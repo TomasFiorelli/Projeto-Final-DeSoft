bf503b705bfa076d0711a7428c7410a488915111
Tomás Fiorelli Barbosa <62888275+TomasFiorelli@users.noreply.github.com>
2020-06-01 11:45:37 -0300

    Update Projeto Final.py
    - Contador
    - Vidas
    - Bloco desaparece quando erra
    - Câmera

--- Projeto Final.py
+++ Projeto Final.py
@@ -15,9 +15,10 @@ pygame.display.set_caption('Jogo de Empilhar Blocos')
 img = pygame.image.load('bloco.png').convert()
 BLOCO_WIDTH = 70
 BLOCO_HEIGHT = 70
+SPEED_CAM = 10
 
 class Bloco(pygame.sprite.Sprite):
-    def __init__(self, img, largura):
+    def __init__(self, img, largura, camera):
         pygame.sprite.Sprite.__init__(self)
         
         img = pygame.transform.scale(img, (largura, BLOCO_HEIGHT))
@@ -27,10 +28,11 @@ class Bloco(pygame.sprite.Sprite):
         self.rect.y = 0
         self.speedx = 5
         self.speedy = 0
+        self.camera = camera
         
     def update(self):
         self.rect.x += self.speedx
-        self.rect.y += self.speedy
+        self.rect.y += self.speedy + self.camera.speedy
         if self.rect.left > WIDTH:
             self.rect.x = -100
             self.rect.y = 0
@@ -49,14 +51,23 @@ class Bloco(pygame.sprite.Sprite):
         self.rect.x = left
         self.rect.y = y
         
+class Camera:
+    def __init__(self):
+        self.speedy = 0
+
 game = True
 clock = pygame.time.Clock()
 FPS = 60
 
-bloco = Bloco(img, BLOCO_WIDTH)
+camera = Camera()
+
+ultimo_bloco_parado = None
+bloco = Bloco(img, BLOCO_WIDTH, camera)
 all_sprites = pygame.sprite.Group()
 all_sprites.add(bloco)
 all_blocks = pygame.sprite.Group()
+contador = 0
+vidas =3
 
 # ===== Loop principal =====
 while game:
@@ -64,15 +75,30 @@ while game:
     
     hits = pygame.sprite.spritecollide(bloco, all_blocks, False)
     if bloco.rect.bottom == HEIGHT or len(hits) > 0:
-        if len(hits) > 0:
-            bloco_colisao = hits[0]
-            bloco.corta(bloco_colisao.rect.left, bloco_colisao.rect.right)
+        if contador == 0:
+            if bloco.rect.bottom == HEIGHT:
+                contador +=1
+        else:
+            if bloco.rect.bottom == HEIGHT:
+                vidas -=1
+                bloco.kill()
+            if len(hits) > 0:
+                bloco_colisao = hits[0]
+                bloco.corta(bloco_colisao.rect.left, bloco_colisao.rect.right)
+                contador += 1
+            
         bloco.speedx = 0
         bloco.speedy = 0
+        ultimo_bloco_parado = bloco
         all_blocks.add(bloco)
         largura = bloco.rect.width
-        bloco = Bloco(img, largura)
+        bloco = Bloco(img, largura, camera)
         all_sprites.add(bloco)
+    
+    if ultimo_bloco_parado is not None and ultimo_bloco_parado.rect.top <  HEIGHT / 2:
+        camera.speedy = SPEED_CAM
+    else:
+        camera.speedy = 0
 
     # ----- Trata eventos
     for event in pygame.event.get():
@@ -90,6 +116,18 @@ while game:
     # ----- Gera saídas
     window.fill((255, 255, 255))  # Preenche com a cor branco
     all_sprites.draw(window)
+    
+    # Desenhando o score
+    text_surface = pygame.font.Font(None, 42).render("{:01d}".format(contador), True, (0, 0, 0))
+    text_rect = text_surface.get_rect()
+    text_rect.midtop = (550,  10)
+    window.blit(text_surface, text_rect)
+    
+    # Desenhando o vida
+    text_surface = pygame.font.Font(None, 42).render("{:01d}".format(vidas), True, (0, 0, 0))
+    text_rect = text_surface.get_rect()
+    text_rect.midtop = (550,  40)
+    window.blit(text_surface, text_rect)
     
 
     # ----- Atualiza estado do jogo
