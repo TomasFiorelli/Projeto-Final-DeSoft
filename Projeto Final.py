518c7351126979b19411932e1d19b3f4278fcfd1
gabriellakz <62666958+gabriellakz@users.noreply.github.com>
2020-06-15 10:32:45 -0300

    Arquivo que contêm o código inteiro junto

--- Projeto Final.py
+++ Projeto Final.py
@@ -235,4 +235,4 @@ while state != QUIT:
 
 
 # ===== Finalização =====
-pygame.quit()  # Função do PyGame que finaliza os recursos utilizados
+pygame.quit()  # Função do PyGame que finaliza os recursos utilizados 
