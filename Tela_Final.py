ea6ec5f25b85689b7029ccc91f8708010a4bbbeb
Tomás Fiorelli Barbosa <62888275+TomasFiorelli@users.noreply.github.com>
2020-06-15 10:22:34 -0300

    Arquivo da tela inicial

---
+++ Tela_Final.py
@@ -0,0 +1,35 @@
+import pygame
+from config import GAME_SCREEN, QUIT
+
+
+def game_over_screen(window, contador): # Função que chama a tela final
+    while True:
+        # ----- Trata eventos
+        for event in pygame.event.get():
+            # ----- Verifica consequências
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_SPACE:
+                    return GAME_SCREEN
+                if event.key == pygame.K_ESCAPE:
+                    return QUIT
+            if event.type == pygame.QUIT:
+                return QUIT
+
+        # Desenha uma imagem de fundo e escrve a pontuação final       
+        game_over_foto = pygame.image.load('img/Foto final.png').convert()        
+        
+        if contador == 1:
+            text2 = 'bloco'
+        else:
+            text2 = 'blocos'
+
+        font = pygame.font.SysFont(None, 80)
+        text1 = font.render('{}'.format(contador), True, (255, 255, 255))
+        text2 = font.render('{}'.format(text2), True, (255, 255, 255))
+        
+        window.fill((0, 0, 0))
+        window.blit(game_over_foto,(0, 0))
+        window.blit(text1, (250, 425))
+        window.blit(text2, (180, 475))
+        
+        pygame.display.update()
