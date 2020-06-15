8fed8d9a927c3f2eb0ff7f88b15d77da673b6acf
PedroZequi <62962214+PedroZequi@users.noreply.github.com>
2020-06-15 10:27:36 -0300

    Add files via upload
    Arquivo com as configurações gerais do jogo

---
+++ config.py
@@ -0,0 +1,19 @@
+# ===== Configurações Gerais =====
+
+# Dados gerais do jogo.
+WIDTH = 600 # Largura da tela
+HEIGHT = 800 # Altura da tela
+BOTTOM = HEIGHT - 50 # Altura em que o chao aparece
+BOTTOM2 = - 2066 # Altura inicial da tela fundo
+
+# Define tamanhos
+BLOCO_WIDTH = 100
+BLOCO_HEIGHT = 70
+SPEED_CAM = 10 # Valor da velocidade da camera
+
+# Estados para controle do fluxo da aplicação
+QUIT = 0
+GAME_INICIO = 1
+GAME_INFORMATIVO = 2
+GAME_SCREEN = 3
+GAME_OVER = 4
